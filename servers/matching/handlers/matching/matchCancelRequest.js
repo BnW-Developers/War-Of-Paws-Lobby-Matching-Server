3e8b8a006ec69b1d42f1380727d1e5f7bd247d99
Minu Han <mwj1205@naver.com>
2024-12-05 00:57:34 +0900

    feat: 매칭 요청, 매칭 취소 요청 핸들러 작동
    - 매칭 요청, 취소 핸들러 작동하도록 로직 추가

--- servers/matching/handlers/matching/matchCancelRequest.js
+++ servers/matching/handlers/matching/matchCancelRequest.js
@@ -1,15 +1,31 @@
 import { handleErr } from '../../../../common/error/handlerErr.js';
+import redisClient from '../../../../common/redis/redisClient.js';
 import logger from '../../../../common/utils/logger/logger.js';
+import { createServerPacket } from '../../../../common/utils/packet/createPacket.js';
+import matchingSystem from '../../matchingQueue/matchingSystem.js';
 
 const matchCancelRequest = async (socket, clientKey, payload) => {
   try {
     logger.info(`match cancel request from: ${clientKey}`);
-    // const user = userSessionManager.getUserBySocket(socket);
-    // if (!user) {
-    //   throw new Error('유저를 찾을 수 없습니다');
-    // }
-    // matchingSystem.removeUser(user.getUserId(), user.getCurrentSpecies());
-    // logger.info(`match Cancel request id: ${user.getUserId()}`);
+
+    // Redis에서 사용자 세션 확인
+    const sessionKey = `user:session:${clientKey}`;
+    const userSession = await redisClient.hgetall(sessionKey);
+
+    if (!userSession) {
+      throw new Error('Match request error: 유저를 찾을 수 없습니다.');
+    }
+
+    // 매칭 중인 경우에만 큐에서 제거
+    if (userSession.isMatchmaking === 'true') {
+      await matchingSystem.removeUser(clientKey, userSession.currentSpecies);
+      logger.info(`match Cancel request for socket: ${clientKey}`);
+
+      const packet = createServerPacket(9, clientKey, { opponentId: 'match cancel' });
+      socket.write(packet);
+    } else {
+      console.warn(`client ${clientKey}: 매칭 중이 아닙니다.`);
+    }
   } catch (err) {
     handleErr(socket, err);
   }
--- servers/matching/handlers/matching/matchRequest.js
+++ servers/matching/handlers/matching/matchRequest.js
@@ -1,32 +1,38 @@
 import { handleErr } from '../../../../common/error/handlerErr.js';
+import redisClient from '../../../../common/redis/redisClient.js';
 import logger from '../../../../common/utils/logger/logger.js';
 import { createServerPacket } from '../../../../common/utils/packet/createPacket.js';
+import matchingSystem from '../../matchingQueue/matchingSystem.js';
 
 const matchRequest = async (socket, clientKey, payload) => {
   try {
     const { species } = payload;
-    console.log('clientKey: ', clientKey);
-    console.log('species: ', species);
-    // const user = userSessionManager.getUserBySocket(socket);
-    // if (!user) {
-    //   throw new Error('유저를 찾을 수 없습니다');
-    // }
-    // logger.info(`match request id: ${user.getUserId()}`);
 
-    // // 유저가 매칭을 돌리고 있는데 또 매칭 요청을 보냈다면
-    // if (user.getIsMatchmaking()) {
-    //   // 매칭 큐에서 유저 삭제
-    //   await matchingSystem.removeUser(user.getUserId(), user.getCurrentSpecies());
-    // }
+    // Redis에서 사용자 세션 확인
+    const sessionKey = `user:session:${clientKey}`;
+    const userSession = await redisClient.hgetall(sessionKey);
 
-    // const result = await matchingSystem.addQueue(user, species);
-    // if (result.success) {
-    //   logger.info(`user ${user.getUserId()} add queue result: ${result.message}`);
-    // } else {
-    //   throw new Error(`user ${user.getUserId()} add queue result: failed`);
-    // }
+    if (!userSession) {
+      throw new Error('Match request error: 유저를 찾을 수 없습니다.');
+    }
 
-    const packet = createServerPacket(9, clientKey, { opponentId: 'yourId' });
+    logger.info(`match request id: ${clientKey}`);
+
+    // 유저가 매칭을 돌리고 있는데 또 매칭 요청을 보냈다면
+    if (userSession.isMatchmaking === 'true') {
+      // 그냥 다음 매칭 요청은 무시
+      return;
+    }
+
+    const result = await matchingSystem.addQueue(clientKey, species);
+
+    if (result.success) {
+      logger.info(`user ${clientKey} add queue result: ${result.message}`);
+    } else {
+      throw new Error(`user ${clientKey} add queue result: failed`);
+    }
+
+    const packet = createServerPacket(9, clientKey, { opponentId: 'tryMatch' });
     socket.write(packet);
   } catch (err) {
     handleErr(socket, err);

92330f7cf139946e727e07036dbcf702cad8d3df
Minu Han <mwj1205@naver.com>
2024-12-12 21:26:56 +0900

    fix: redis expire에 key 빼먹어서 발생한 오류 수정

--- servers/lobby/events/C2Lobby.event.js
+++ servers/lobby/events/C2Lobby.event.js
@@ -50,13 +50,13 @@ class C2LEventHandler {
       if (!socket.isAuthenticated) {
         // TODO: 첫 패킷에서 보낸 토큰 꺼내오기
         const { packetType, payload } = parseC2LobbyPacket(socket);
-	console.log(`packetType: ${packetType}, payload: ${payload}`);
+        console.log(`packetType: ${packetType}, payload: ${payload}`);
         if (packetType !== PACKET_TYPE.AUTH_REQUEST) {
           return;
         }
 
         const { token } = getDecodedPayload(packetType, payload);
-	console.log(`token: ${token}`);
+        console.log(`token: ${token}`);
         if (token) {
           try {
             // JWT 토큰 검증
@@ -124,7 +124,7 @@ class C2LEventHandler {
 
       const sessionKey = `user:session:${key}`;
       redisClient.hset(sessionKey, 'isLoggedIn', false);
-      redisClient.expire(300);
+      redisClient.expire(sessionKey, 300);
 
       // 유저 접속 종료 Pub
       redisClient.publish(
